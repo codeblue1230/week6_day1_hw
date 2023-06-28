6fe8b4e356beda897eaa4706b170cdd7d866de82
Marc Engle <marcengle30@gmail.com>
2023-06-28 08:55:00 -0400

    day 2 hw commit

--- my_inventory/__init__.py
+++ my_inventory/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask
 from config import Config
 from .site.routes import site
 from .authentication.routes import auth
+from .models import db as root_db
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -9,4 +11,7 @@ app.register_blueprint(site)
 app.register_blueprint(auth)
 
 app.config.from_object(Config)
+
+root_db.init_app(app)
+migrate = Migrate(app, root_db)
 
--- my_inventory/authentication/routes.py
+++ my_inventory/authentication/routes.py
@@ -1,9 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from my_inventory.forms import UserLoginForm
+from my_inventory.models import User, db
 
 
 auth = Blueprint('authentication', __name__, template_folder='auth')
 
 
-@auth.route('/auth')
+@auth.route('/auth', methods = ['GET', 'POST'])
 def home():
-    return 'You made it to my other blueprint page. Congratulations'
+    userform = UserLoginForm()
+
+    try:
+        if request.method == 'POST' and userform.validate_on_submit():
+            email = userform.email.data
+            username = userform.username.data
+            password = userform.password.data
+            print(email, password)
+
+            user = User(email, username, password)
+
+            db.session.add(user)
+            db.session.commit()
+
+            flash(f'You have successfully created a user account {email}', 'user-created')
+
+            return redirect(url_for('site.home'))
+
+        
+    except:
+        raise Exception('Invalid Form Data. Please Check your form')
+    
+    return render_template('signup.html', form=userform)
