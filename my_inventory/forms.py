6e55e28740186d4619211ea9338026783151e308
Marc Engle <marcengle30@gmail.com>
2023-06-29 21:59:56 -0400

    first glitch test

--- my_inventory/forms.py
+++ my_inventory/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, DecimalField, IntegerField
 from wtforms.validators import DataRequired, Email
 
 
@@ -7,4 +7,14 @@ class UserLoginForm(FlaskForm):
     username = StringField('username', validators = [DataRequired()])
     email = StringField('email', validators = [DataRequired(), Email()])
     password = PasswordField('password', validators = [DataRequired()])
+    submit_button = SubmitField()
+
+
+class LegoForm(FlaskForm):
+    name = StringField('name')
+    description = StringField('description')
+    price = DecimalField('price', places=2)
+    difficulty = StringField('difficulty')
+    piece_count = IntegerField('piece_count')
+    lego_category = StringField('lego_category')
     submit_button = SubmitField()
--- my_inventory/site/routes.py
+++ my_inventory/site/routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import login_required, current_user
+from my_inventory.forms import LegoForm
+from my_inventory.models import Lego, db
 
 
 site = Blueprint('site', __name__, template_folder='site_templates')
@@ -8,6 +11,32 @@ site = Blueprint('site', __name__, template_folder='site_templates')
 def home():
     return render_template('index.html')
 
-@site.route('/profile')
+@site.route('/profile', methods=['GET', 'POST'])
+@login_required
 def profile():
-    return render_template('profile.html')
+    legoform = LegoForm()
+
+    try:
+        if request.method == 'POST' and legoform.validate_on_submit():
+            name = legoform.name.data
+            description = legoform.description.data
+            price = legoform.price.data
+            difficulty = legoform.price.data
+            piece_count = legoform.piece_count.data
+            lego_category = legoform.lego_category.data
+            user_token = current_user.token
+
+            lego = Lego(name, description, price, difficulty, piece_count, lego_category, user_token)
+
+            db.session.add(lego)
+            db.session.commit()
+
+            return redirect(url_for('site.profile'))
+        
+    except:
+        raise Exception('Lego set could not be created. Please try again.')
+    
+    user_token = current_user.token
+    legos = Lego.query.filter_by(user_token=user_token)
+
+    return render_template('profile.html', form=legoform, legos=legos)
